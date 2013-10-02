aab2dd63d379b4848c7ca329f5f382f96164414e
telgniw <b97902044@ntu.edu.tw>
2013-10-02 09:50:54 +0800

    Add a faster searcher.

--- searcher.py
+++ searcher.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import cv2, numpy
+from util import blur
 
 class Searcher(object):
     def search(self, img, start_point):
@@ -31,7 +32,29 @@ class BFSearcher(Searcher):
                 queue.append((y, x-1))
             if check(y, x+1):
                 queue.append((y, x+1))
+            if check(y-1, x):
+                queue.append((y-1, x))
 
             img[x][y], res[x][y] = 0, (204, 204, 255)
 
         return res, (y, x)
+
+class FastSearcher(Searcher):
+    def __init__(self, searcher):
+        self.previous_img, self.previous_point = None, None
+        self.searcher = searcher
+
+    def search(self, img, start_point):
+        # set negative values to 0
+        if self.previous_img is None:
+            diff = img
+        else:
+            p_img = blur(self.previous_img)
+            _, p_img = cv2.threshold(p_img, 2, 255, cv2.THRESH_BINARY)
+
+            diff = (img - p_img).clip(min=0)
+            start_point = self.previous_point
+
+        res, end_point = self.searcher.search(diff, start_point)
+        self.previous_img, self.previous_point = numpy.copy(img), end_point
+        return res, end_point
