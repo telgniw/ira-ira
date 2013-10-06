24ac339b25b22e165205b110a866aaf13d203258
godgunman <godgunman@gmail.com>
2013-10-06 11:46:31 +0800

    play sound when game strart

--- game.py
+++ game.py
@@ -2,7 +2,7 @@
 import cv, cv2, numpy
 from lib.color_filter import ColorFilter
 from lib.searcher import CheatSearcher
-from lib.util import bounding_rect, crop
+from lib.util import bounding_rect, crop, play_sound
 from lib.window import Window
 from pump import *
 
@@ -14,11 +14,13 @@ class Game(object):
     
     def start(self, video):
         ps = PumpSpark()
+        '''
         ps.pump2([
           ([(1, 254), (4, 254)], 5), 
           ([(0, 254), (2, 254)], 1), 
           ([(2, 80), (5, 80)], 10),
           ([(1, 254), (4, 254)], 5)])  
+        '''
         window = Window('Water Ira-Ira Bou')
         mask_window = Window('Mask')
 
@@ -43,7 +45,7 @@ class Game(object):
         bar_index = 0
         water_index = 0
         start = False
-        
+        print self.color_range_bar
         while True:
             key = window.wait()
 
@@ -65,15 +67,16 @@ class Game(object):
             water_index, (x, y) = searcher_water.search(mask_water, water_index)
             cv2.circle(current_img, (x, y), 3, colors[0], thickness=2)
 
-            mask_window.draw(mask_water)
+            mask_window.draw(mask_bar)
             window.draw(current_img)
-            '''
+            
             if bar_index != 0 and start == False:
+              play_sound('sound/button-8.wav')
               start = True
               ps.turnOff(1, 4)
               ps.pump([(0, 254), (3,254)], 0.5)
               ps.turnOn([(2, 70), (5, 70)])
-            '''
+            
         mask_window.close()
         window.close()
         ps.turnOff()
